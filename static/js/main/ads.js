87570f11e05294baaa8b0993b557ea4c97ada372
meet447 <51074036+meet447@users.noreply.github.com>
2024-02-03 19:45:07 +0530

    html fix ads

---
+++ static/js/main/ads.js
@@ -0,0 +1,10 @@
+// ads.js
+
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6864078994533800';
+    script.crossorigin = 'anonymous';
+    document.head.appendChild(script);
+  })();
+  
