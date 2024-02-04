932443b38fac687775df19b3ba521818206105e0
ApoorvaDeval0601 <apoorvamdeval@gmail.com>
2024-02-04 16:44:41 +0530

    change in config.py

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
-# config.py
+#config.py
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 firebaseConfig = {
-  "apiKey": "AIzaSyCkP7g1w7NH6oz6aCg-HrllRKAaoxRQoa8",
-  "authDomain": "chipling-ai.firebaseapp.com",
-  "databaseURL": "https://chipling-ai-default-rtdb.firebaseio.com/",
-  "projectId": "chipling-ai",
-  "storageBucket": "chipling-ai.appspot.com",
-  "messagingSenderId": "782885159174",
-  "appId": "1:782885159174:web:68e1ddf46e89703370de87",
-  "measurementId": "G-0444GXLW5F",
+    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+    "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+    "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+    "appId": os.getenv("FIREBASE_APP_ID"),
+    "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
+    "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
 }
